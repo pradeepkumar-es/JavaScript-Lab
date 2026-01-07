87f0e09e17d8756a09dc4a346e62a7e082ac6693
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-07 17:00:04 +0530

    Update comments for variable visibility explanation
    Clarified comments regarding variable visibility and temporal dead zone.

--- AdvancedWorkingInFunctions/10IsVaraibleVisible.js
+++ AdvancedWorkingInFunctions/10IsVaraibleVisible.js
@@ -9,6 +9,7 @@ function func() {
 func();
 /*
 line 4 will output Reference error: variable x can not be accessed before intialization,
-when function is executed, it's lexical environment have variable x and it will use from there not from outer lexical environment and known to engine but it is unintialized state until let comes hence it can be used before intialization
+when function is executed, it's lexical environment have variable x and it will use from there not from outer lexical environment and known to engine but it is unintialized state until let comes 
+(also called temporal dead zone).  hence it can be used before intialization
 if we remove line 6 let x = 2, then x will be accesed from outer lexical environment and there will be no error
 */
