e0caeca661fb12cd5ed3578c23509cc94ee6e496
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-07 15:10:37 +0530

    Implement greeting function with variable scope
    Add a function that alerts a greeting using a variable.

---
+++ AdvancedWorkingInFunctions/5LatestChange.js
@@ -0,0 +1,12 @@
+let name = "John";
+
+function sayHi() {
+  alert("Hi, " + name);
+}
+
+name = "Pete";
+
+sayHi(); // what will it show: "John" or "Pete"?
+/*
+since function is executed after the name variable update hence it will take latest value from own lexical environment or outer
+*/
