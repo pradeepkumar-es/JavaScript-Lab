8ac92dcc534e8346dfc9a0ae430def856d994082
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-07 16:07:51 +0530

    Update comment for counter object behavior
    Clarified comment on output behavior of counter functions.

--- AdvancedWorkingInFunctions/8CounterObject.js
+++ AdvancedWorkingInFunctions/8CounterObject.js
@@ -15,5 +15,5 @@ alert( counter.up() ); // ?
 alert( counter.up() ); // ?
 alert( counter.down() ); // ?
 /*
-line 14, 15, and 16 will output 1, 2, and 1 respectively bcz both nested function is created using the same lexical environment and share access to same variable count.
+line 14, 15, and 16 will output 1, 2, and 1 respectively bcz both nested function is created using the same outer lexical environment and share access to same variable count.
 */
