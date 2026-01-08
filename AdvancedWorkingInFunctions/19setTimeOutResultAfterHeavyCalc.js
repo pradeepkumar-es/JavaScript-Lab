0058efee2cf42801a70aa075c3b6ec576d83f861
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-08 23:52:22 +0530

    Update comments on setTimeout and loop execution
    Clarify comments regarding setTimeout behavior and loop execution.

--- AdvancedWorkingInFunctions/19setTimeOutResultAfterHeavyCalc.js
+++ AdvancedWorkingInFunctions/19setTimeOutResultAfterHeavyCalc.js
@@ -7,3 +7,4 @@ for(let j = 0; j < 100000000; j++) {
   i++;
 }
 //since every setTimeout run after current code execution finished so loop will update i to 100000000, so after 100ms of loop finish, it will alert 100000000
+//timer will start at the moment of the call of setTimeout, mean while loop start and if takes longer than 100ms then after loop setTimeout will alert immediately after 
