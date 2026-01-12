1af87fba9f923688b4b52d7cadeed20169f78886
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-12 21:25:47 +0530

    Update comments on prototype and object references
    Clarify comments regarding prototype behavior and object references.

--- AdvancedWorkingWithObjects/5ChangingPrototype3.js
+++ AdvancedWorkingWithObjects/5ChangingPrototype3.js
@@ -11,3 +11,7 @@ alert( rabbit.eats ); // ?
 //line 10 will be false, bcz when rabbit object is created, this happen => rabbit.[[prototype]] -> Rabbit.prototype which has {eats: true}
 //so internally rabbit -> [[prototype]]->{eats:true}, since in question value eats is directly updated, hence rabbit.eats will so same updated value bcz rabbit object and function object both
 //are referring to the same value
+
+// Objects are assigned by reference. The object from Rabbit.prototype is not duplicated, it’s still a single object referenced both by Rabbit.prototype and by the [[Prototype]] of rabbit.
+
+// So when we change its content through one reference, it is visible through the other one.
