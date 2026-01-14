6259c5893987be5a71cf5710dc1206db7d192689
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-14 21:12:03 +0530

    Implement defer method for delayed function calls
    Added defer method to Function prototype for delayed execution.

---
+++ AdvancedWorkingWithObjects/8DecoratingToAddedBuiltInMethodToFunction.js
@@ -0,0 +1,21 @@
+/*
+Add to the prototype of all functions the method defer(ms), that returns a wrapper, delaying the call by ms milliseconds.
+
+Here’s an example of how it should work:
+
+function f(a, b) {
+  alert( a + b );
+}
+
+f.defer(1000)(1, 2); // shows 3 after 1 second
+Please note that the arguments should be passed to the original function.
+*/
+function f(a, b) {
+  alert( a + b );
+}
+Function.prototype.defer = function(ms){
+    return function(a,b){
+        setTimeout(f,ms,a,b)
+    }
+}
+f.defer(1000)(1, 2); // shows 3 after 1 second
