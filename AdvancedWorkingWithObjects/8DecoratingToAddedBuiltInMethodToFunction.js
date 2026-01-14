1b4a98bebb8c94ddadb484e120d17370ab5b6208
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-14 21:16:51 +0530

    Add defer method to Function prototype

--- AdvancedWorkingWithObjects/8DecoratingToAddedBuiltInMethodToFunction.js
+++ AdvancedWorkingWithObjects/8DecoratingToAddedBuiltInMethodToFunction.js
@@ -18,4 +18,12 @@ Function.prototype.defer = function(ms){
         setTimeout(f,ms,a,b)
     }
 }
+
+//other approach
+// Function.prototype.defer = function(ms){
+//   let f = this;
+//   return function(...args){
+//     setTimeout(()=>f.apply(this,args),ms);
+//   }
+// }
 f.defer(1000)(1, 2); // shows 3 after 1 second
