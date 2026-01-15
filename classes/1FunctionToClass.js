a57013f4e53b4f360671442bf380344523fbcc3a
Pradeep Kumar <102599114+pradeepkumar-es@users.noreply.github.com>
2026-01-15 16:36:49 +0000

    classes

---
+++ classes/1FunctionToClass.js
@@ -0,0 +1,95 @@
+/*
+
+<!doctype html>
+<script>
+
+  function Clock({ template }) {
+  
+    let timer;
+  
+    function render() {
+      let date = new Date();
+  
+      let hours = date.getHours();
+      if (hours < 10) hours = '0' + hours;
+  
+      let mins = date.getMinutes();
+      if (mins < 10) mins = '0' + mins;
+  
+      let secs = date.getSeconds();
+      if (secs < 10) secs = '0' + secs;
+  
+      let output = template
+        .replace('h', hours)
+        .replace('m', mins)
+        .replace('s', secs);
+  
+      console.log(output);
+    }
+  
+    this.stop = function() {
+      clearInterval(timer);
+    };
+  
+    this.start = function() {
+      render();
+      timer = setInterval(render, 1000);
+    };
+  
+  }
+  
+  let clock = new Clock({template: 'h:m:s'});
+  clock.start();
+  clock.stop();
+
+</script>
+
+</html>
+Rewrite to class
+importance: 5
+The Clock class (see the sandbox) is written in functional style. Rewrite it in the “class” syntax.
+
+P.S. The clock ticks in the console, open it to see.
+*/
+
+class Clock {
+    // timer; //we can skip it 
+    // tempalte; 'h:m:s' //we can skip it, bcz class properties do not need to be declared ahead of time in the class,
+    //we can create them whenever we assign them to this 
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
