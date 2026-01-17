3d51637defa51df47f442e5b8efc7eb870f7cb84
pradeepkumar-es <createthefuture121@gmail.com>
2026-01-17 19:23:05 +0530

    start of the Promise, Async/Await

---
+++ PromisesAsyncAwait/1ResolvePromise.js
@@ -0,0 +1,12 @@
+/*
+Re-resolve a promise?
+What’s the output of the code below?
+
+let promise = new Promise(function(resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(alert);
+*/
